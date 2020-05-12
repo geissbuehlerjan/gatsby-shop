da54962ac7b3d7cd631ed187dd9dc2ac8fc8ed71
jangbhlr <geissbuehlerjan@gmail.com>
2020-05-12 17:38:12 -0500

    domain

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -14,7 +14,7 @@ export default function Product({ product }) {
             data-item-id={product.id}
             data-item-name={product.title}
             data-item-price={product.price}
-            data-item-url="https://unterbachnertuurli.netlify.app/"
+            data-item-url="https://unterbachnertuurli.netlify.app/#!/"
             data-item-image={product.image.fluid.src}
           >
             Buchen
