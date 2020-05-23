cc21dad803e4fb8ff7ad883ab1671c601a2e23da
jangbhlr <geissbuehlerjan@gmail.com>
2020-05-23 16:32:15 -0500

    lastone

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,10 +27,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-snipcart",
       options: {
-        apiKey:
-          "OTA4YTc4YWQtMzQzNS00YzM5LWFkNmUtZWVmNzIyY2M4Yjg3NjM3MjQ4NzkzNTk0Mzc2NDg4",
-
-        //process.env.SNIPCART_API,
+        apiKey: process.env.SNIPCART_API,
         autopop: true,
       },
     },
